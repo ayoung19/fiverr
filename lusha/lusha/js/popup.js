72ca934a13ecf242c356a4084cf98fe340a9dfb6
linglingandy <zeusandninja@gmail.com>
2020-09-24 23:34:28 -0700

    v2

--- lusha/lusha/js/popup.js
+++ lusha/lusha/js/popup.js
@@ -4,6 +4,10 @@ chrome.storage.sync.get("profile-data", function (result) {
     document.querySelector("#name").innerHTML = `${data.person.firstname} ${data.person.lastname}`;
     document.querySelector("#email-value").innerHTML = `${data.person.email}`;
     document.querySelector("#phone-value").innerHTML = `${formatPhoneNumber(data.person.phone)}`
+    document.querySelector("#company").innerHTML = `${data.company.name}`
+    document.querySelector("#website-value").innerHTML = `${data.company.companyUrl}`
+    document.querySelector("#website-value").href = `https://${data.company.companyUrl}`
+    document.querySelector("#founded").innerHTML = `${data.company.founded}`
 });
 
 function formatPhoneNumber(phoneNumberString) {
