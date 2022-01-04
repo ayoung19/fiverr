aea4ab7fe594fb2dbb99f3541703df1727fd5ee2
ayoung19 <zeusandninja@gmail.com>
2022-01-03 17:02:24 -0800

    Delete clipboard history code

--- auto-refresher/webpack.common.js
+++ auto-refresher/webpack.common.js
@@ -3,37 +3,37 @@ const path = require("path");
 module.exports = {
   entry: {
     popup: path.join(__dirname, "src/popup/index.tsx"),
-    eventPage: path.join(__dirname, "src/eventPage.ts")
+    background: path.join(__dirname, "src/background/index.tsx"),
   },
   output: {
     path: path.join(__dirname, "dist/js"),
-    filename: "[name].js"
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader"
+        use: "ts-loader",
       },
       {
         exclude: /node_modules/,
         test: /\.scss$/,
         use: [
           {
-            loader: "style-loader" // Creates style nodes from JS strings
+            loader: "style-loader",
           },
           {
-            loader: "css-loader" // Translates CSS into CommonJS
+            loader: "css-loader",
           },
           {
-            loader: "sass-loader" // Compiles Sass to CSS
-          }
-        ]
-      }
-    ]
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  }
+    extensions: [".ts", ".tsx", ".js"],
+  },
 };
