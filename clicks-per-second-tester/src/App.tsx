ea55b251a83fca3f85afde859cf8c3576dbd2d4b
ayoung <zeusandninja@gmail.com>
2022-01-12 01:36:03 -0800

    Finish ui for cps tester

--- clicks-per-second-tester/src/App.tsx
+++ clicks-per-second-tester/src/App.tsx
@@ -1,33 +1,82 @@
 import React, { FC, useEffect, useState } from "react";
+import {
+  EuiPanel,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiButton,
+  EuiStat,
+  EuiSpacer,
+} from "@elastic/eui";
 
 export const App: FC = () => {
   const [clicks, setClicks] = useState(0);
-  const [milliseconds, setMilliseconds] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
     if (clicks === 1) {
+      setSeconds((prevState) => prevState + 1 / 120);
       setInterval(() => {
-        setMilliseconds((prevState) => prevState + 1000 / 120);
+        setSeconds((prevState) => prevState + 1 / 120);
       }, 1000 / 120);
     }
   }, [clicks]);
 
   return (
-    <>
-      <button onClick={() => setClicks((prevState) => prevState + 1)}>
-        click
-      </button>
-      <div>clicks: {clicks}</div>
-      <div>seconds: {Math.floor(milliseconds / 10) / 100}</div>
-      <div>
-        clicks per second: {Math.floor((clicks / milliseconds) * 100000) / 100}
-      </div>
-      <a href="https://www.clickspeedtester.com/" target="_blank">
-        button 1
-      </a>
-      <a href="https://www.clickspeedtester.com/spacebar-test/" target="_blank">
-        button 2
-      </a>
-    </>
+    <EuiPanel
+      paddingSize="l"
+      hasBorder={false}
+      hasShadow={false}
+      color="transparent"
+    >
+      <EuiFlexGroup responsive={false}>
+        <EuiFlexItem>
+          <EuiButton
+            onClick={() => setClicks((prevState) => prevState + 1)}
+            fill={true}
+          >
+            {clicks === 0 ? "Click To Start" : "Keep Clicking"}
+          </EuiButton>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiSpacer />
+      <EuiFlexGroup responsive={false}>
+        <EuiFlexItem>
+          <EuiStat title={clicks} description="Clicks" />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiStat title={seconds.toFixed(2)} description="Elapsed Seconds" />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiStat
+            title={(clicks / seconds).toFixed(2)}
+            description="Clicks Per Second"
+            isLoading={seconds === 0}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiSpacer />
+      <EuiFlexGroup responsive={false}>
+        <EuiFlexItem>
+          <EuiButton
+            href="https://www.clickspeedtester.com/"
+            target="_blank"
+            iconType="popout"
+            size="s"
+          >
+            Button 1
+          </EuiButton>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiButton
+            href="https://www.clickspeedtester.com/spacebar-test/"
+            target="_blank"
+            iconType="popout"
+            size="s"
+          >
+            Button 2
+          </EuiButton>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiPanel>
   );
 };
